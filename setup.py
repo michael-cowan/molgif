a57d2600da7c088e7cf7c21d2532ef2a98c55993
michael-cowan <mcowan92@gmail.com>
2019-05-15 18:17:11 -0400

    added readme as long description

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ import setuptools
 with open('molgif/_version.py', 'r') as fid:
     exec(fid.read())
 
+with open('README.md', 'r', encoding='utf-8') as readme:
+    description = readme.read()
+
 setuptools.setup(name='molgif',
                  version=__version__,
                  author='Michael Cowan',
                  url='https://www.github.com/michael-cowan/molgif',
                  description="creates smooth gifs of rotating molecules",
+                 long_description=description,
+                 long_description_content_type='text/markdown'
                  packages=['molgif'],
                  python_requires='>=2.7',
                  install_requires=['matplotlib',
