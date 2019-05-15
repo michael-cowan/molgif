e723de873f7ab19ce1f5631ba30f888edc97edd9
michael-cowan <mcowan92@gmail.com>
2019-05-15 18:24:59 -0400

    long description ignores gifs in readme

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ with open('molgif/_version.py', 'r') as fid:
     exec(fid.read())
 
 with open('README.md', 'r', encoding='utf-8') as readme:
-    description = readme.read()
+    # ignore gifs
+    description = ''.join([i for i in readme.readlines()
+                           if not i.startswith('![')])
 
 setuptools.setup(name='molgif',
                  version=__version__,
@@ -12,7 +14,7 @@ setuptools.setup(name='molgif',
                  url='https://www.github.com/michael-cowan/molgif',
                  description="creates smooth gifs of rotating molecules",
                  long_description=description,
-                 long_description_content_type='text/markdown'
+                 long_description_content_type='text/markdown',
                  packages=['molgif'],
                  python_requires='>=2.7',
                  install_requires=['matplotlib',
